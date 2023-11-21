cdd3f80cfdede859238019ab8f3f0648953fc111
Dwi Rizqi <a.dwirizqi.h123@gmail.com>
2023-11-21 10:40:27 +0700

    add sensor warna

---
+++ Lib/warna-func.c
@@ -0,0 +1,32 @@
+int maxHijau = 215;
+int maxKuning = 100;
+int maxMerah = 180;
+
+int minHijau = 190;
+int minKuning = 0;
+int minMerah = 120;
+
+void bacawarna()
+{
+    nadc7 = read_adc(7);
+    lcd_gotoxy(13, 1);
+    sprintf(buff, "%d   ", nadc7);
+    lcd_puts(buff);
+}
+
+int checkWarna() {
+    int warna;
+    nadc7 = read_adc(7);
+    if(nadc7>minMerah && nadc7<maxMerah){
+        warna = 0;
+    }
+    else if(nadc7>minKuning && nadc7<maxKuning){
+        warna = 1;
+    }
+    else if(nadc7>minHijau && nadc7<maxHijau){
+        warna = 2;
+    }else{
+        warna = 3;
+    }
+    return warna;
+}
